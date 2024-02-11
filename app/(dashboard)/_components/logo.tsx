b4eb4400f40577d57e00af263a488277889a5d02
Tasneem Fandakli <tasnimtepe@gmail.com>
2024-02-11 21:16:28 +0300

    fix logo

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return <Image height={50} width={50} alt="logo" src="/logo.svg" />;
+  return <Image height={30} width={30} alt="logo" src="/logo.svg" />;
 };
