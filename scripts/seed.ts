aacc3523928bfa8072018c85496a9155fd946175
Tasneem Fandakli <tasnimtepe@gmail.com>
2024-02-13 15:34:34 +0300

    create course page with edit mode

---
+++ scripts/seed.ts
@@ -0,0 +1,29 @@
+const { PrismaClient } = require("@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+  const Data = [
+    { name: "Computer Science" },
+    { name: "Music" },
+    { name: "Fitness" },
+    { name: "Photography" },
+    { name: "Accounting" },
+    { name: "Engineering" },
+    { name: "Filming" },
+  ];
+  try {
+    for (const data of Data) {
+      await database.category.create({
+        data: data,
+      });
+    }
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
