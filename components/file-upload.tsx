e62b2e9b5eb4c8cc75624faa30f2a9ec1d22af6d
Tasneem Fandakli <tasnimtepe@gmail.com>
2024-02-13 15:30:22 +0300

    implement uploading images and files using uploadThing

---
+++ components/file-upload.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import toast from "react-hot-toast";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+
+interface FileUploadProps {
+  onChange: (data: { url?: string; name?: string }) => void;
+  endpoint: keyof typeof ourFileRouter;
+}
+
+export const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange({ url: res?.[0].url, name: res?.[0].name });
+      }}
+      onUploadError={(error: Error) => {
+        toast.error(`${error?.message}`);
+      }}
+    />
+  );
+};
