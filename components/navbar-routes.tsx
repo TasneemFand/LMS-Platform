9148ffba86907331a291bd5c74687397bc1eb3c3
Tasneem Fandakli <tasnimtepe@gmail.com>
2024-03-03 09:21:14 +0300

    Revert "teacher admin protection of api and layout"
    This reverts commit e27c9f2de07e1699f61730242c288b1ff39402e4.

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { UserButton, useAuth } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import { SearchInput } from "./search-input";
-import { isTeacher } from "@/lib/teacher";
 
 export const NavbarRoutes = () => {
-  const { userId } = useAuth();
-
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
@@ -32,13 +29,13 @@ export const NavbarRoutes = () => {
               Exit
             </Button>
           </Link>
-        ) : isTeacher(userId) ? (
+        ) : (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Teacher mode
             </Button>
           </Link>
-        ) : null}
+        )}
         <UserButton afterSignOutUrl="/" />
       </div>
     </>
